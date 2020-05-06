17cbc7402e176d1422a73329d10cc7312907f27a
Crafter-deo <adesolaadeoluwa@gmail.com>
2020-05-07 00:57:30 +0100

    Second commit

--- CsharpTaskFour/Controllers/SqrtController.cs
+++ CsharpTaskFour/Controllers/SqrtController.cs
@@ -1,15 +1,26 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CsharpTaskFour.Controllers
 {
     public class SqrtController : Controller
     {
-        
+        [HttpGet]
         public ActionResult Compare()
         {
             return View();
         }
         
-        
+        [HttpPost]
+        public ActionResult Compare(string firstNumber, string secondNumber)
+        {
+            double firstNumbersqrt = Math.Sqrt(double.Parse(firstNumber));
+            double secondNumbersqrt = Math.Sqrt(double.Parse(secondNumber));
+
+            double result = Math.Max(firstNumbersqrt, secondNumbersqrt);
+
+            ViewBag.Result = result;
+            return View();
+        }
     }
 }
