744e7ee50f2f8f1d646b5064c3b892d697bc636b
Crafter-deo <adesolaadeoluwa@gmail.com>
2020-05-06 21:45:15 +0100

    initial commit

---
+++ CsharpTaskFour/Controllers/SqrtController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CsharpTaskFour.Controllers
+{
+    public class SqrtController : Controller
+    {
+        
+        public ActionResult Compare()
+        {
+            return View();
+        }
+        
+        
+    }
+}
